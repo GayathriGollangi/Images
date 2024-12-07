6d5f08ecdc103b20eadef684b4c86b17f93f1d06
gayathrigollangi <gayathrigollangii@gmail.com>
2024-12-07 21:46:02 +0530

    Images Gallery

---
+++ my-app/src/Components/ToggleTheme.jsx
@@ -0,0 +1,19 @@
+import { UseGlobalContext } from '../Context/Context'
+import { BsFillMoonFill, BsFillSunFill } from "react-icons/bs";
+import style from '../CSS/ToggleTheme.module.css';
+
+const ToggleTheme = () => {
+    const { isDarkTheme,toggleDarkTheme } = UseGlobalContext();
+  return (
+    <div className={style["toggle-container"]}>
+        <button className={style["toggle-dark"]} onClick={toggleDarkTheme}>
+      {
+        isDarkTheme ? (<BsFillMoonFill className={style["toggle-icon"]} />): 
+        (<BsFillSunFill className={style["toggle-icon"]}/>)
+      }
+      </button>
+    </div>
+  )
+}
+
+export default ToggleTheme
---
+++ my-app/src/Context/Context.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+
+export const AppContext = createContext();
+
+const getInitialMode = ()=>{
+    const preferMode = window.matchMedia('(prefers-color-scheme)').matches;
+    const storedMode =localStorage.getItem('darkMode');
+    if(storedMode === null){
+        return preferMode
+    }
+    return storedMode === true;
+}
+
+export const AppProvider = ({children})=>{
+     
+    const [isDarkTheme, setIsDarkTheme] = useState(getInitialMode());
+    const [searchTerm ,setSearchTerm] = useState("cat");
+
+    const toggleDarkTheme = ()=>{
+        setIsDarkTheme(!isDarkTheme);
+        localStorage.setItem("darkMode",isDarkTheme);
+    }
+    useEffect(()=>{
+        document.body.classList.toggle("dark-theme",isDarkTheme);
+
+    },[isDarkTheme])
+    return(
+        <AppContext.Provider value={{isDarkTheme,toggleDarkTheme,searchTerm,setSearchTerm}}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export const UseGlobalContext = ()=>{
+    return useContext(AppContext);
+}
